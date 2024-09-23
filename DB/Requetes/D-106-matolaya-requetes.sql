9554e850f778539c6adbab497a9085d483d1fad8
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2024-09-23 13:03:38 +0200

    feat(DB): Ajout de nouvelles explications sur les requêtes SQL.

--- DB/Requetes/D-106-matolaya-requetes.sql
+++ DB/Requetes/D-106-matolaya-requetes.sql
@@ -65,3 +65,49 @@ explication détaillée de celle-ci.*/
     GROUP BY idJoueur --Puis regrouper les lignes qui ont des valeurs identiques de la colonne idJoueur.
     ORDER BY PrixTotalDepense --Ordonner tout ce résultat dans l'ordre croissant (défault) en fonction de PrixTotalDepense.
     DESC LIMIT 10; --Et finalement, limiter le nombre de résultat à 10.
+
+/*Requêtes 7 sur 10 : 
+    Récupérez tous les joueurs et leurs commandes, même s'ils n'ont pas passé de commande.
+    Dans cet exemple, même si un joueur n'a jamais passé de commande, il sera quand
+    même listé, avec des valeurs `NULL` pour les champs de la table `t_commande`.*/
+
+    SELECT jou.*, co.* --Permet de sélectionner tous les élements de la table joueur et de la table commande. '*' signifie tout. 'jou' et 'co' sont les diminutifs des tables.
+    FROM t_joueur jou --Permet de spécifier de quelles tables proviennent ces élements, en l'occurence, uniquement t_joueur que l'on renomme en jou pour un accès simplifié.
+    LEFT JOIN t_commande co --Permet de sélectionner tous les élements n'appartenant pas qu'exclusivement à t_commande que l'on renomme en co.
+    ON jou.idJoueur = co.fkJoueur --Seulement lorsque l'id du joueur est égal à la clé étrangère de la commande.
+    LEFT JOIN t_detail_commande dc --Comme précédemment, tous les éléments n'appartenant pas qu'exclusivement à t_detail_commande que l'on renomme en dc.s
+    ON co.idCommande = dc.fkCommande; --Seulement quand l'id de la commande est égal à la clé étramgère de détail de la commande.
+
+/*Requêtes 8 sur 10 : 
+    Récupérer toutes les commandes et afficher le pseudo du joueur s?il existe, sinon afficher `NULL` pour le pseudo.*/
+
+     SELECT co.*, jou.* --Permet de sélectionner tout les élements de la table commande et de la table joueur.
+     FROM t_commande co --Spécifier la table d'ou provient les élements, en l'occurence, t_commande que l'on renomme en co.
+     LEFT JOIN t_joueur jou --Sélectionner tous les élemets n'appartenant pas qu'exclusivement à t_joueur que l'on renomme en jou.
+     ON co.fkJoueur = jou.idJoueur; --Seulement quand la clé étrangère du joueur est égale à l'id du joueur.
+
+/*Requêtes 9 sur 10 : 
+    Trouver le nombre total d'armes achetées par chaque joueur (même si ce joueur n'a acheté aucune Arme).*/
+
+    SELECT jouPseudo, --Sélectionner le pseudo du joueur.
+    IFNULL(SUM(dc.detQuantiteCommande), 0) AS ArmeTotalesAchetees --Sélectionner la somme de quantitée commandée de la table detailCommande seulement si la valeur n'est pas null, sinon lui attribuer 0. Puis la renommer en ArmeTotalesAchetees
+    FROM t_joueur jou --Spécifier de quelle table proviennent les éléments.
+    LEFT JOIN t_commande c --Sélectionner tous les élemets n'appartenant pas qu'exclusivement à t_commande que l'on renomme en 'c'.
+    ON c.fkJoueur = jou.idJoueur --Seulement quand la clé étrangère du joueur est égale à l'id du joueur.
+    LEFT JOIN t_detail_commande dc  --Sélectionner tous les élemets n'appartenant pas qu'exclusivement à t_detail_commande que l'on renomme en dc.
+    ON dc.fkCommande = c.idCommande --Seulement quand la clé étrangère de detail commande est égale à l'id de la commande.
+    GROUP BY jou.jouPseudo; --Puis regrouper les lignes qui ont des valeurs identiques de la colonne jouPseudo.
+
+/*Requêtes 10 sur 10 : 
+    Trouver les joueurs qui ont acheté plus de 3 types d'armes différentes*/
+    SELECT jou.jouPseudo, --Sélectionner le pseudo du joueur.
+    COUNT(DISTINCT(dc.fkArme)) as ArmeDifferenteAchetees --Compter le nombre d'id d'arme différentes et le renommer en armeDifferentesAchetees. 
+    FROM t_joueur jou --Spécifier de quelle table.
+    LEFT JOIN t_commande c --Sélectionner tous les élemets n'appartenant pas qu'exclusivement à t_commande que l'on renomme en c.
+    ON c.fkJoueur = jou.idJoueur --Seulement quand la clé étrangère du joueur est égale à l'id du joueur.
+    LEFT JOIN t_detail_commande dc --Pareil, mais pour la table t_detail_commande renommée en dc.
+    ON dc.fkCommande = c.idCommande --Pareil, mais pour les attributs fkCommande et idCommande.
+    LEFT JOIN t_arme arm --Pareil, mais pour la table t_arme renommée en arm.
+    ON arm.idArme = dc.fkArme --Pareil, mais pour les attributs idArme et fkArme.
+    GROUP BY jou.jouPseudo --Puis regrouper les lignes qui ont des valeurs identiques de la colonne jouPseudo.
+    HAVING ArmeDifferenteAchetees > 3; --Seulement quand le nombre de ligne est suppérieur à 3.
