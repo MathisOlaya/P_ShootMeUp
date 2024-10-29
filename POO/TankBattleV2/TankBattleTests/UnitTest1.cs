75bd7811b2a137f8b0c4c205bca9cedde50294f8
MathisOlaya <150446507+MathisOlaya@users.noreply.github.com>
2024-10-29 17:49:20 +0100

    feat(POO): Création des Tests

---
+++ POO/TankBattleV2/TankBattleTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using TankBattleV2;
+
+namespace TankBattleTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PlayerIsMovingToLeftWhen_A_IsPressed()
+        {
+            //Arrange
+            
+
+            //Act
+
+            //Assert
+        }
+    }
+}
